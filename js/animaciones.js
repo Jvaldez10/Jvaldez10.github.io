15def1408ab2d80a830d930185d96c7b4e72137d
JVALDEZ <JVALDEZ@JVALDEZH>
2017-06-08 09:21:19 -0500

    fdghj

---
+++ js/animaciones.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JVALDEZ on 7/06/2017.
+ */
+
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
